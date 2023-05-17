62f6549710e920d09f32095f487c6e4908e9ced3
Krishna Surakanti <krishna.surakanti@kellogg.northwestern.edu>
2023-05-17 00:34:01 +0000

    added index,new views and controllers for each

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,26 @@
 class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+   def new
+     @post = Post.new
+  #   # render posts/new view with new Post form
+   end
+
+   def create
+  #   # start with a new Post
+     @post = Post.new
+  #   # assign user-entered form data to Post's columns
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["body"]
+    @post["image"] = params["post"]["image"]
+  #   # save Post row
+  @post.save
+  #   # redirect user
+  redirect_to "/posts"
+   end
+  
+
 end
