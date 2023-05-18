7716191c7d3d34fbe16f8b1c529fbc1dd6e584da
Krishna Surakanti <krishna.surakanti@kellogg.northwestern.edu>
2023-05-18 22:50:17 +0000

    edited controllers

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,30 @@
 class PlacesController < ApplicationController
+
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+    @post = Post.find_by({"id" => @place["post_id"]})
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+
+    @place = Place.new
+
+    # assign user-entered form data to Contact's columns
+    @place["name"] = params["place"]["name"]
+
+    # assign relationship between Contact and Company
+    @place["post_id"] = params["place"]["post_id"]
+
+    # save Contact row
+    @place.save
+
+    # redirect user
+    redirect_to "/posts/#{@place["post_id"]}"
+  end
+  
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,9 +13,7 @@ class PostsController < ApplicationController
   #   # start with a new Post
      @post = Post.new
   #   # assign user-entered form data to Post's columns
-    @post["author"] = params["post"]["author"]
-    @post["body"] = params["post"]["body"]
-    @post["image"] = params["post"]["image"]
+    @post["name"] = params["post"]["name"]
   #   # save Post row
   @post.save
   #   # redirect user
