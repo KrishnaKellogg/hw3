ae43bbad226ad62ae09d945951de8e5d2ab29551
Krishna Surakanti <krishna.surakanti@kellogg.northwestern.edu>
2023-05-19 03:08:09 +0000

    closer to linking posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,24 +2,26 @@ class PostsController < ApplicationController
 
    def new
      @post = Post.new
+     @post.place_id = params["place_id"]
   #   # render posts/new view with new Post form
    end
 
    def create
   #   # start with a new Post
      @post = Post.new
-  #   # assign user-entered form data to Post's columns
-    @post["title"] = params["post"]["title"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post["description"] = params["post"]["description"]
-    
-    # assign relationship between Contact and Company
-    @post["place_id"] = params["post"]["place_id"]
 
-    @post.save
+  #   # assign user-entered form data to Post's columns
+      @post["title"] = params["post"]["title"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["description"] = params["post"]["description"]
+    
+    # assign relationship between Posts and Places
+      @post["place_id"] = params["post"]["place_id"]
+
+      @post.save
  
   #   # redirect user
-  redirect_to "/places/#{@post["place_id"]}"
+    redirect_to "/places/#{@post.place_id}"
    end
    
   end
